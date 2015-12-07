d15ca29f08adb641f81e19f3bb42467c247be06c
aqutw <aquarian.ex+github@gmail.com>
2015-12-07 21:25:46 +0800

    finish setup and configurate webpack-dev-server

--- app/index.js
+++ app/index.js
@@ -1,5 +1,5 @@
 var sub = require('./sub');
 var app  = document.createElement('div');
-app.innerHTML = '<h1>Hello World Test</h1>';
+app.innerHTML = '<h1>Hello World Test2222</h1>';
 app.appendChild(sub());
 document.body.appendChild(app);
