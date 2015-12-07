b5c2df39872c30f1d87c5542d7811d69b7647f18
aqutw <aquarian.ex+github@gmail.com>
2015-12-07 21:41:44 +0800

    Added jQuery & moment 3rd-part libraries

--- app/index.js
+++ app/index.js
@@ -1,7 +1,12 @@
 require('./main.scss');
 
+var $ = require('jquery');
+var moment = require('moment');
+
 var sub = require('./sub');
 var app  = document.createElement('div');
 app.innerHTML = '<h1>Hello World Test2222</h1>';
 app.appendChild(sub());
 document.body.appendChild(app);
+
+$('body').append('<p>look at me! now is ' + moment().format() + '</p>');
